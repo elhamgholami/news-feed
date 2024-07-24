ade68aa94c8f36177bfd63c9b2e1646e8342c928
elham <elham_gholami@comp.iuct.ac.ir>
2024-07-24 19:09:47 +0330

    filtering articles with and without images and adding some styles

--- src/components/TopHeadlines.tsx
+++ src/components/TopHeadlines.tsx
@@ -1,44 +1,70 @@
-'use client';
+"use client";
 
 import { useEffect, useState } from "react";
-import axios from 'axios';
-
+import axios from "axios";
+import styles from "../styles/TopHeadlines.module.scss";
 interface Article {
-    title: string;
-    description: string;
-  }
-
-const TopHeadlines = () =>
-{
-    const [articles, setArticles] = useState<Article[]>([]);
-    useEffect(() => {
-        const fetchTopHeadlines = async () => {
-          try {
-            const response = await axios.get('https://newsapi.org/v2/top-headlines', {
-              params: {
-                country: 'us',
-                category: 'science',
-
-                apiKey: '83ff284543f441809114d438ca5dc54b',
-              },
-            });
-            setArticles(response.data.articles);
-          } catch (error) {
-            console.error('Error fetching top headlines:', error);
-          } 
-        };
-    
-        fetchTopHeadlines();
-      }, []);
-    return (
-        <div>
-          {articles.map((article, index) => (
-            <div key={index}>
-              <h2>{article.title}</h2>
-              <p>{article.description}</p>
-            </div>
-          ))}
-        </div>
-      );
+  title: string;
+  description: string;
+  urlToImage: string;
 }
+
+const TopHeadlines = () => {
+  const [articles, setArticles] = useState<Article[]>([]);
+  const articlesWithImages = articles.filter((article) => article.urlToImage);
+  const articlesWithoutImages = articles.filter(
+    (article) => !article.urlToImage
+  );
+  useEffect(() => {
+    const fetchTopHeadlines = async () => {
+      try {
+        const response = await axios.get(
+          "https://newsapi.org/v2/top-headlines",
+          {
+            params: {
+              country: "us",
+              category: "science",
+
+              apiKey: "83ff284543f441809114d438ca5dc54b",
+            },
+          }
+        );
+        setArticles(response.data.articles);
+      } catch (error) {
+        console.error("Error fetching top headlines:", error);
+      }
+    };
+
+    fetchTopHeadlines();
+  }, []);
+  return (
+    <div className={styles.horizontalHeadlines}>
+      <div className={styles.verticalHeadlines}>
+        {articlesWithImages.map((article, index) => (
+          <div key={index} className={styles.article}>
+            <img
+              src={article.urlToImage}
+              alt={article.title}
+              className={styles.image}
+            />
+            <div className={styles.content}>
+              <h2 className={styles.title}>{article.title}</h2>
+              <p className={styles.description}>{article.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className={styles.verticalHeadlines}>
+        {articlesWithoutImages.map((article, index) => (
+          <div key={index} className={styles.article}>
+            <div className={styles.content}>
+              <h2 className={styles.title}>{article.title}</h2>
+              <p className={styles.description}>{article.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default TopHeadlines;
