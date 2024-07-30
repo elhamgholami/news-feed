84e77758332f1d75d67c3699e9d6a84ed34bb2b2
elham <elham_gholami@comp.iuct.ac.ir>
2024-07-30 18:04:54 +0330

    adding Zod and react hook form

--- src/app/Search/index.tsx
+++ src/app/Search/index.tsx
@@ -1,27 +1,45 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import React, { useState } from "react";
-
+import { SubmitHandler, useForm } from "react-hook-form";
+import z from "zod";
 interface Props {
-  
-  setSearchQuery:any;
-
+  setSearchQuery: any;
 }
+const searchSchema = z.object({
+  searchText: z.string().min(1, "Enter something to search!"),
+});
+type searchSchemaType = z.infer<typeof searchSchema>;
 
 const Search = ({ setSearchQuery }: Props) => {
   const [searchText, setSearchText] = useState("");
-  const handleSearch = (e: React.FormEvent) => {
-    e.preventDefault();
-    setSearchQuery(searchText)
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<searchSchemaType>({
+    resolver: zodResolver(searchSchema),
+  });
 
+  const onSubmit: SubmitHandler<searchSchemaType> = (
+    data: searchSchemaType
+  ) => {
+    setSearchQuery(data.searchText);
+  };
   return (
     <div>
-      <form onSubmit={handleSearch}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"
           value={searchText}
           placeholder="Search for news..."
+          {...register("searchText")}
+          onChange={(e) => setSearchText(e.target.value)}
+          disabled={isSubmitting}
         />
         <button type="submit">Search</button>
+        {errors.searchText && !isSubmitting && (
+          <p>{errors.searchText.message}</p>
+        )}
       </form>
     </div>
   );
